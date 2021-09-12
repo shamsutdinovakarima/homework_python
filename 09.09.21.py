fe1d47ad882cd13adb75f61330b9cd27573cedaf
shamsutdinovakarima <89980487+shamsutdinovakarima@users.noreply.github.com>
2021-09-12 14:35:44 +0300

    дз генератор случайного имени

---
+++ 09.09.21.py
@@ -0,0 +1,3 @@
+import random
+surnames_list=['Dora Miller','David Cook','Jennifer Turner','Jose Stephens','Charlene Parker','Nora Malone','Walter Gibson','Monica Marsh','William Anderson']
+print( random.choice(surnames_list))
